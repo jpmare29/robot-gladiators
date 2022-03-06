a400b7e012ee3ad3094c5016a8f7c2f2693d8fac
jpmare29 <jpmmare@yahoo.com>
2022-03-06 15:21:41 -0500

    touch ups

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
+var playerHealth = 36;
 var playerAttack = 10;
 var playerMoney = 10;
 
@@ -64,9 +64,15 @@ var fight = function(enemyName) {
     } // end of while loop
   }; // end of fight function
   
-  
+
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 20;
-    fight(pickedEnemyName);
-  }
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 20;
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
+    }
